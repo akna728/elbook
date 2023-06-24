e1725f155b56f3a61616428141789000288c3f5a
akna728 <nako.akashi728@gmail.com>
2023-06-24 16:15:38 +0900

    コントローラクラスちょっと書いた

--- ELBOOK2023/src/main/java/com/example/demo/controller/BookController.java
+++ ELBOOK2023/src/main/java/com/example/demo/controller/BookController.java
@@ -1,19 +1,39 @@
 package com.example.demo.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.BookList;
 import com.example.demo.service.BookService;
 
 import lombok.Data;
 
-@Controller
+@RestController
 @Data
 @RequestMapping("/book")
 public class BookController {
 	
 	@Autowired
 	BookService bookService;
+	
+	@GetMapping("/bookList")
+	public String bookInfo(Model model) {
+		model.addAttribute("BookList", bookService.findBook());
+		return "bookinfo";
+	}
+	
+	public BookController(BookService bookService) {
+		this.bookService = bookService;
+	}
+	
+	@GetMapping
+	public List<BookList> getAllBooks(){
+		return bookService.findBook();
+	}
 
 }
--- ELBOOK2023/src/main/java/com/example/demo/service/BookService.java
+++ ELBOOK2023/src/main/java/com/example/demo/service/BookService.java
@@ -1,8 +1,25 @@
 package com.example.demo.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.BookList;
+import com.example.demo.mapper.BookMapper;
 
 @Service
 public class BookService {
+	
+	@Autowired
+	BookMapper bookMapper;
+	
+	public List<BookList> findBook(){
+		return bookMapper.findBook();
+	}
+	
+	public List<BookList> getBookList(int rental_key_id){
+		return bookMapper.getBookList(rental_key_id);
+	}
 
 }
